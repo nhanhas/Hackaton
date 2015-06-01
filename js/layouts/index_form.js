8395ce10388e6f22695df7574d3387360cd48c8d
MPereira <mpereira132@hotmail.com>
2015-06-01 23:35:23 +0100

    #S01
    Very first commit

---
+++ js/layouts/index_form.js
@@ -0,0 +1,10 @@
+/* This class will show the popup loading */
+
+ $("#btn").click(function(event){
+	event.preventDefault();
+	 
+	
+	//$('#body').fadeOut(500);
+
+	$('.container').addClass('form-success');
+});
